7835111f4689d6dc79ea0ca678bfc1e18c798a65
andrewzl <andrewzouli@gmail.com>
2022-03-26 14:33:20 -0400

    changing pin map to json for compatability with circuitpython

--- code.py
+++ code.py
@@ -1,4 +1,9 @@
+"""
+Main Code for Widget Lab 3
+"""
+
 import board
+import microcontroller
 import digitalio
 import pwmio
 
@@ -8,13 +13,14 @@ import adafruit_hcsr04
 import time
 from utils.init import init_dc, init_stepper
 
-import yaml
+import json
 
 
 def grasp():
     # continuously run and adjust grasp intensity based on encoder values
     # once hit good encoder range, run cap off
     # once cap off is complete, run release
+    print('grasping')
     def cap_off():
         pass
     pass
@@ -26,12 +32,11 @@ def release():
 
 if __name__ == '__main__':
 
-    with open('config.yml', 'r') as file:
-        config = yaml.safe_load(file)
-    
+    with open('config.json', 'r') as file:
+        config = json.load(file)
+
     # Initialize DC Motor Grasp
-    # in1_grasp, in2_grasp, ena_grasp = init_dc(config['grasp']['dir1_pin'], config['grasp']['dir2_pin'], config['grasp']['ena_pin'])
-    in1_grasp, in2_grasp, ena_grasp = init_dc(board.D7, board.D8, board.D11)
+    in1_grasp, in2_grasp, ena_grasp = init_dc(eval(config['grasp']['dir1_pin']), eval(config['grasp']['dir2_pin']), eval(config['grasp']['ena_pin']))
     
     # Initialize DC Motor Load
     in1_load, in2_load, ena_load = init_dc(board.D4, board.D5, board.D6)
@@ -41,16 +46,16 @@ if __name__ == '__main__':
     step_pin, dirn_pin = init_stepper(board.D2, board.D3, 0.005, 200)
 
     # Initialize Sensing
-    prox_sensor = adafruit_hcsr04.HCSR04(trigger_pin=board.A1, echo_pin=board.A2)
+    prox_sensor = adafruit_hcsr04.HCSR04(eval(config['ultrasonic']['trig_pin']), eval(config['ultrasonic']['echo_pin']))
 
     # Main Loop
     while True:
+        print(prox_sensor.distance)
         # while bottle is not present
         if prox_sensor.distance > config['presence_thresh']:
             continue
         else: # bottle is present and close enough to grasp
             grasp()
-        
-     
+        time.sleep(1)
         
         
