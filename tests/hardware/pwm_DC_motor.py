c167ac735455beddfd3a694f1d17774eaea84849
Michal Davis <michalkdavis@gmail.com>
2022-03-26 16:16:55 -0400

    Move motor to B output
    For physical wiring routing. Also, updated pins to be correct

--- tests/hardware/pwm_DC_motor.py
+++ tests/hardware/pwm_DC_motor.py
@@ -7,38 +7,47 @@ import time
 import digitalio
 import pwmio
 import sys
-from adafruit_motor import stepper
 
-# set up logic inputs to motor driver ("step" and "pulse") as outputs
-in1 = digitalio.DigitalInOut(board.D7)
-in2 = digitalio.DigitalInOut(board.D8)
-in1.direction = digitalio.Direction.OUTPUT
-in2.direction = digitalio.Direction.OUTPUT
 
-# set up PWM output to motor driver
-ena = pwmio.PWMOut(board.D11)
+dir1_pin = board.D10
+dir2_pin = board.D11
+en_pin = board.D9
+
+in3 = digitalio.DigitalInOut(dir1_pin)
+in4 = digitalio.DigitalInOut(dir2_pin)
+
+in3.direction = digitalio.Direction.OUTPUT
+in4.direction = digitalio.Direction.OUTPUT
+
+enb = pwmio.PWMOut(en_pin)
 
 # set initial duty cycle, direction, and step commands
-ena.duty_cycle = 0
-in1.value = False
-in2.value = True
+enb.duty_cycle = 0
+in3.value = False
+in4.value = True
 
 start_time = time.time()
 time_limit = 9
 while True:
     # rotate motor clockwise
-    in1.value, in2.value = (False, True)
-    ena.duty_cycle = 40000
+    in3.value, in4.value = (False, True)
+    enb.duty_cycle = 40000
     print("Motor is rotating CW")
     time.sleep(5)
 
     # rotate motor counterclockwise
-    in1.value, in2.value = (True, False)
+    in3.value, in4.value = (True, False)
     print("Motor is rotating CCW")
-    ena.duty_cycle = 50000
+    enb.duty_cycle = 50000
     time.sleep(5)
 
     # check if we've been doing this for more than the time limit
     total_time = time.time() - start_time
     if total_time > time_limit:
         break
+
+'''
+def run_motor(clockwise, duty_cycle):
+    in3.value, in4.value (not clockwise, clockwise)
+    enb.duty_cycle = duty_cycle
+'''
